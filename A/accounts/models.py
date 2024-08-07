7ac5d4a751ee909d7e717e2e644fba2c236cd19d
MEHRSHAD-OSG <95499396+MEHRSHAD-OSG@users.noreply.github.com>
2024-08-07 03:23:31 -0700

    models.py

--- A/accounts/models.py
+++ A/accounts/models.py
@@ -2,15 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
 
-# class User(AbstractUser):
-#
-#     address = models.CharField(max_length=200)
-#     phone_number = models.CharField(max_length=11,unique=True)
-#
-#     USERNAME_FIELD = 'username'
-#     objects = UserManager()
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User,models.CASCADE,related_name='uprofile')
     age = models.PositiveSmallIntegerField(default=1)
