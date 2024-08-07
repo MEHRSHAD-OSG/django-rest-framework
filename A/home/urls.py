bfd8b5a660fbcfb2ef6ca356963a9dbba6b67c40
MEHRSHAD-OSG <95499396+MEHRSHAD-OSG@users.noreply.github.com>
2024-08-07 03:28:03 -0700

    urls.py

--- A/home/urls.py
+++ A/home/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    # path('', views.Home.as_view(), name='home'),
     path('', views.QuestionView.as_view(), name='question'),
     path('question/create/', views.QuestionCreateView.as_view(), name='question_create'),
     path('question/update/<int:pk>/', views.QuestionUpdateView.as_view(), name='question_update'),
