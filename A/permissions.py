98b77160ca7a90c76831966b01bdd87eb779cbe5
MEHRSHAD-OSG <95499396+MEHRSHAD-OSG@users.noreply.github.com>
2024-08-07 03:18:35 -0700

    permissions.py

--- A/permissions.py
+++ A/permissions.py
@@ -1,13 +1,9 @@
-"""
-for customize permissions
-"""
+
 from rest_framework.permissions import BasePermission , SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
     message = "not yours"
-    # after user enter to view
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        # کاربری کع اومده باید اون چیز برای خودش باشه تا بتونه تغییر بده
         return obj.user == request.user
